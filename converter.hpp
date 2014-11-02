cd056b8ebf5f629e0890f434de91de488144aae7
vanessavj <vanessa.vj@web.de>
2014-11-02 16:30:36 +0100

    test 2

--- converter.hpp
+++ converter.hpp
@@ -10,7 +10,6 @@ class converter
     converter();
     virtual double convert(double inValue) = 0;
     //test to see if it works
-    //apparently it doesn't
 
     virtual std::string toString() const = 0;
     virtual void print() const = 0;
