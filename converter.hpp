0555093fe648d05aa6167cc2959b1a3765717095
vanessavj <vanessa.vj@web.de>
2014-11-02 16:26:42 +0100

    test

--- converter.hpp
+++ converter.hpp
@@ -9,6 +9,9 @@ class converter
   public:
     converter();
     virtual double convert(double inValue) = 0;
+    //test to see if it works
+    //apparently it doesn't
+
     virtual std::string toString() const = 0;
     virtual void print() const = 0;
 };
